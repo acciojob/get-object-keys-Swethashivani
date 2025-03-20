b9b6bccfde5d54234587f062a01648efda9aa6a7
Swethashivani <40227788+Swethashivani@users.noreply.github.com>
2025-03-20 18:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//your JS code here. If required.
-const student = {
-  name: "John",
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
 };
 
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
-}
+// Creating an object
+const student = {
+    name: "John"
+};
 
 // Testing the function
-console.log(getKeys(student));
+console.log(student.getKeys());  // Output: ["name"]
